6211b9dd58eb72c82eae66317d3fcf1a0a24eee1
Ovis De Los Santos <124535584+Delosovi@users.noreply.github.com>
2024-01-26 11:16:45 -0500

    Create MyProfile.java

---
+++ MyProfile.java
@@ -0,0 +1,9 @@
+public class MyProfile {
+	public static void main(String[] args) {   
+    String name = "Ovis";
+    int age = 30;
+    double desiredSalary = 180000;
+    char gender = 'm';
+    boolean lookingForJob = true;
+	}
+}
