8651158f7b7eb43c08c83c7e6bdcfaf1be89f35b
Ovis De Los Santos <124535584+Delosovi@users.noreply.github.com>
2025-06-16 12:48:38 -0400

    Create Block Letter.py

---
+++ Block Letter.py
@@ -0,0 +1,26 @@
+# Ovis De Los Santos
+# Gaming
+# My Version
+print(" OOO " + "DDDD ")
+print("O   O"+ "D   D")
+print("O   O"+ "D   D")
+print("O   O"+ "D   D")
+print("O   O"+ "D   D")
+print("O   O"+ "D   D")
+print(" OOO "+ "DDDD ")
+
+#Improved Version
+def print_initials():
+    initials = [
+        " OOO   DDDD ",
+        "O   O  D   D",
+        "O   O  D   D",
+        "O   O  D   D",
+        "O   O  D   D",
+        "O   O  D   D",
+        " OOO   DDDD "
+    ]
+    for line in initials:
+        print(line)
+
+print_initials()
